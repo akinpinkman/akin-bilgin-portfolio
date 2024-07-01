9ba0768ed2ac1997d5429a0eb0d70d6c5231c93d
akinpinkman <akinbilgin54@hotmail.com>
2024-07-02 01:13:18 +0300

    Add article section
    Wrapped <p> tags with <article> tag

--- src/components/childComps/BenKimim.tsx
+++ src/components/childComps/BenKimim.tsx
@@ -13,37 +13,43 @@ export function BenKimim() {
           alt="Akın Bilgin"
           width={500}
         />
-        {/*  */}
-        <p className="">
-          Merhabalar!
+
+        <article className="">
+          <p>Merhabalar!</p>
           <br />
           <br />
-          Ben Akın Bilgin, bilgiye aç olan ve kendini sürekli geliştirmeye
-          adamış bir Frontend Developer'ım. Bülent Ecevit Üniversitesi İngiliz
-          Dili ve Edebiyatı bölümünden mezun oldum ve şuanda Anadolu
-          Üniversitesinde Yönetim Bilişim Sistemleri eğitimi almaktayım. 2 yılı
-          aşkın bir süredir yazılım alanında kendimi durmadan eğitiyor, günlük
-          sorunlarımı çözen ve üretme açlığımı dindiren projeler geliştiriyorum.
-          Teknolojiler kısmında şuanda bildiğim ve kullandığım dilleri
-          (frameworkleri) görebilirsiniz. Orada gördüklerinizin dışında
-          Typescript ve Jest teknolojilerini öğrenmekte oldukça hevesliyim.
-          Önceki çalıştığım şirketlerdeki çalışma arkadaşlarımla İngilizce
-          iletişim kurmamıza rağmen ortaya harika bir takım çalışması
-          koyabiliyorduk.
+          <p>
+            Ben Akın Bilgin, bilgiye aç olan ve kendini sürekli geliştirmeye
+            adamış bir Frontend Developer'ım. Bülent Ecevit Üniversitesi İngiliz
+            Dili ve Edebiyatı bölümünden mezun oldum ve şuanda Anadolu
+            Üniversitesinde Yönetim Bilişim Sistemleri eğitimi almaktayım. 2
+            yılı aşkın bir süredir yazılım alanında kendimi durmadan eğitiyor,
+            günlük sorunlarımı çözen ve üretme açlığımı dindiren projeler
+            geliştiriyorum. Teknolojiler kısmında şuanda bildiğim ve kullandığım
+            dilleri (frameworkleri) görebilirsiniz. Orada gördüklerinizin
+            dışında Typescript ve Jest teknolojilerini öğrenmekte oldukça
+            hevesliyim. Önceki çalıştığım şirketlerdeki çalışma arkadaşlarımla
+            İngilizce iletişim kurmamıza rağmen ortaya harika bir takım
+            çalışması koyabiliyorduk.
+          </p>
           <br />
           <br />
-          Bu yüzden elbette klişe gelecek ancak benim için her zaman takım
-          çalışması ve iletişim birçok şeyin önünde gelmektedir. Bundan önce
-          çalıştığım iş yerlerinde de edindiğim tek kriter buydu diyebilirim.
-          Öğrenmekten ve üzerime yerinde sorumluluk almaktan çekinmeyen
-          birisiyim. Gelecek sorunları öngörebilir ve buna göre çözümler
-          üretebilirim. Bütün bunların dışında iş dünyasında sıkca kullanılan
-          Slack, Discord, Trello, Asana, Jira, ve ClickUp gibi uygulamalara
-          oldukça hakimim.
+          <p>
+            Bu yüzden elbette klişe gelecek ancak benim için her zaman takım
+            çalışması ve iletişim birçok şeyin önünde gelmektedir. Bundan önce
+            çalıştığım iş yerlerinde de edindiğim tek kriter buydu diyebilirim.
+            Öğrenmekten ve üzerime yerinde sorumluluk almaktan çekinmeyen
+            birisiyim. Gelecek sorunları öngörebilir ve buna göre çözümler
+            üretebilirim. Bütün bunların dışında iş dünyasında sıkca kullanılan
+            Slack, Discord, Trello, Asana, Jira, ve ClickUp gibi uygulamalara
+            oldukça hakimim.
+          </p>
           <br />
           <br />
-          Umarım birlikte çalışabiliriz. En kısa sürede görüşmek üzere 👋.
-        </p>
+          <p>
+            Umarım birlikte çalışabiliriz. En kısa sürede görüşmek üzere 👋.
+          </p>
+        </article>
         <Iletisim />
       </div>
     </section>
