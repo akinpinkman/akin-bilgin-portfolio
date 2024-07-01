e78d3981d20c77c7b0387e240eb0899b9819bc13
akinpinkman <akinbilgin54@hotmail.com>
2024-07-02 01:39:00 +0300

    Removed ''CSS" from hero section

--- src/components/childComps/TextGenerateEffect.tsx
+++ src/components/childComps/TextGenerateEffect.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { TextGenerateEffect } from "../ui/text-generate-effect";
 
-const words = `2021 yılından beri karmaşık sorunları basit, anlaşılır ve güzel tasarımlara döküyorum. Responsive ve kullanıcı dostu arayüzler oluştururken göz alıcı CSS animasyonları kullanarak bu deneyimi taçlandırıyorum.`;
+const words = `2021 yılından beri karmaşık sorunları basit, anlaşılır ve güzel tasarımlara döküyorum. Responsive ve kullanıcı dostu arayüzler oluştururken göz alıcı animasyonlar kullanarak bu deneyimi taçlandırıyorum.`;
 
 export function TextAnimation() {
   return <TextGenerateEffect words={words} />;
